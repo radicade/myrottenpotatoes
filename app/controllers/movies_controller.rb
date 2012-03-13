a460b685a020e1860379f87afd2d0585bc61725f
David Choi <radicade@hotmail.com>
2012-03-13 06:48:14 +0000

    part4

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,15 +6,55 @@ class MoviesController < ApplicationController
     # will render app/views/movies/show.<extension> by default
   end
 
+  @all_ratings = ['G', 'PG', 'PG-13', 'R']
+#  @checked_ratings = {"G"=>"1", "PG"=>"1", "PG-13"=>"1", "R"=>"1"}
+
   def index
-    if(params[:id] == "title_header")
-       @movies = Movie.all.sort_by{|x| x.title}
-    elsif(params[:id] == "release_date_header")
-       @movies = Movie.all.sort_by{|x| x.release_date}
-    else
-       @movies = Movie.all
-    end
+
+#  @checked_ratings = {"G"=>"1", "PG"=>"1", "PG-13"=>"1", "R"=>"1"}
+#    p "initial @checked_ratings = " , @checked_ratings
+#    p "Params[:ratings] = ", params[:ratings]
+
     @hilite_status = params[:id]
+    @all_ratings = ['G', 'PG', 'PG-13', 'R']
+#    @checked_ratings = params[:ratings]
+#    if(param[:commit] == 'Refresh')
+       
+
+    if !(params[:ratings] == NIL)
+#      p "hello world"
+     @checked_ratings = params[:ratings]
+#      @checked_ratings = {"G"=>"1", "PG"=>"1", "PG-13"=>"1", "R"=>"1"}
+    end 
+    p @checked_ratings
+
+    if !(@checked_ratings == NIL) 
+      @movies = Movie.where(:rating => @checked_ratings.keys)
+    else
+      @movies = Movie.all
+#      @movies = [] 
+    end
+
+    if(params[:id] == "title_header")
+#       @movies = Movie.all.sort_by{|x| x.title}
+       @movies = @movies.sort_by{|x| x.title}
+    elsif(params[:id] == "release_date_header")
+#       @movies = Movie.all.sort_by{|x| x.release_date}
+       @movies = @movies.sort_by{|x| x.release_date}
+#    else
+#       @movies = Movie.all
+    end
+#    if !(@checked_ratings == NIL)
+#       @checked_ratings.each {|checked_rating, value|
+#         p checked_rating 
+#         @movies = @movies.select{|movie| movie.rating == checked_rating}
+#      #}
+#       @movies = Movie.where(:rating => "PG-13")
+#       @movies = Movie.where(["rating IN (?)", @checked_ratings])
+#       @movies = Movie.where(:rating => @checked_ratings.keys)
+#    end
+
+    #@movies = @movies.select{|movie| movie.rating == "R"}
   end
 
   def new
--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -10,4 +10,15 @@ module MoviesHelper
       return NIL;
     end
   end
+  def check_rating(rating)
+    p "check_rating(", rating, @checked_ratings
+    if(@checked_ratings == NIL)
+       @checked_ratings = {"G" => "1", "PG" => "1", "PG-13" => "1", "R" => "1"}
+       return true;
+    elsif(@checked_ratings[rating] == "1")
+       return true;
+    else
+       return false;
+    end
+  end
 end
